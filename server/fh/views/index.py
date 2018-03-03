fc516e88ed04286b3621f3db74da26d5c28fb65e
Zian Ke <kezian@umich.edu>
2018-03-03 15:51:26 -0500

    flask index render index.html

--- server/fh/views/index.py
+++ server/fh/views/index.py
@@ -5,7 +5,16 @@ import fh.model
 import random
 
 
-@fh.app.route(fh.app.config['APPLICATION_ROOT'], methods=['GET'])
-def index():
+
+@fh.app.route(fh.app.config['APPLICATION_ROOT'], defaults={'path': ''})
+@fh.app.route(fh.app.config['APPLICATION_ROOT'] + '<path:path>')
+def index(path):
     """Display / route."""
     return flask.render_template('index.html')
+
+@fh.app.route(fh.app.config['APPLICATION_ROOT'] + 'static/<path:filename>')
+def file(filename):
+    """Display /static/<path:filename> route."""
+    print('here')
+    return flask.send_from_directory(
+        fh.app.config['STATIC_FOLDER'], filename)
