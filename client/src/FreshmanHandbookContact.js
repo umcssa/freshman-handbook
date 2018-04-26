5c5cbe07d2e7a2665bd97051058f126613b22dc0
kezian <kezian@umich.edu>
2018-04-25 21:51:58 -0400

    finish share link and download pdf

--- client/src/FreshmanHandbookContact.js
+++ client/src/FreshmanHandbookContact.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import ReactStretchableButton from 'react-stretchable-button';
-import Icon from 'antd/lib/icon';
+import {Icon, Modal, Button, Input} from 'antd';
 import QueueAnim from 'rc-queue-anim';
+import {CopyToClipboard} from 'react-copy-to-clipboard';
 
 const contactStyle = {
     position: 'fixed',
@@ -19,23 +21,61 @@ const reactStretchableButtonStyles = {
 };
 
 
-const FreshmanHandbookContact = () => (
-    <div style={contactStyle}>
-        <QueueAnim>
-            <ReactStretchableButton key="wechat" {...reactStretchableButtonStyles}
-                                    componentDisplayed={<Icon type="wechat" style={{fontSize: 22}}/>}
-                                    componentToDisplay={<span style={{whiteSpace: 'nowrap'}}>微信公众号</span>}/>
-            <ReactStretchableButton key="group" {...reactStretchableButtonStyles}
-                                    componentDisplayed={<Icon type="usergroup-add" style={{fontSize: 22}}/>}
-                                    componentToDisplay={<span style={{whiteSpace: 'nowrap'}}>加入新生群</span>}/>
-            <ReactStretchableButton key="share" {...reactStretchableButtonStyles} stretchPadding={20}
-                                    componentDisplayed={<Icon type="share-alt" style={{fontSize: 22}}/>}
-                                    componentToDisplay={<span style={{whiteSpace: 'nowrap'}}>分享本文链接</span>}/>
-            <ReactStretchableButton key="download" {...reactStretchableButtonStyles} stretchPadding={20}
-                                    componentDisplayed={<Icon type="download" style={{fontSize: 22}}/>}
-                                    componentToDisplay={<span style={{whiteSpace: 'nowrap'}}>下载新生手册</span>}/>
-        </QueueAnim>
-    </div>
-);
+export default class FreshmanHandbookContact extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {shareLinkVisible: false};
+    }
 
-export default FreshmanHandbookContact
+    render() {
+        return (
+            <div>
+                <div style={contactStyle}>
+                    <QueueAnim>
+                        <ReactStretchableButton key="wechat" {...reactStretchableButtonStyles}
+                                                componentDisplayed={<Icon type="wechat" style={{fontSize: 22}}/>}
+                                                componentToDisplay={<span style={{whiteSpace: 'nowrap'}}>微信公众号</span>}/>
+                        <ReactStretchableButton key="group" {...reactStretchableButtonStyles}
+                                                componentDisplayed={<Icon type="usergroup-add" style={{fontSize: 22}}/>}
+                                                componentToDisplay={<span style={{whiteSpace: 'nowrap'}}>加入新生群</span>}/>
+                        <CopyToClipboard text={window.location.href}>
+                            <ReactStretchableButton key="share" {...reactStretchableButtonStyles} stretchPadding={20}
+                                                    onClick={() => {
+                                                        console.log(window.location.href);
+                                                        this.setState({shareLinkVisible: true});
+                                                    }}
+                                                    componentDisplayed={<Icon type="share-alt" style={{fontSize: 22}}/>}
+                                                    componentToDisplay={<span
+                                                        style={{whiteSpace: 'nowrap'}}>分享本文链接</span>}/>
+                        </CopyToClipboard>
+                        <ReactStretchableButton key="download" {...reactStretchableButtonStyles} stretchPadding={20}
+                                                onClick={() => {
+                                                    window.location.href = '/freshman-handbook/uploads/新生手册.pdf/';
+                                                }}
+                                                componentDisplayed={<Icon type="download" style={{fontSize: 22}}/>}
+                                                componentToDisplay={<span
+                                                    style={{whiteSpace: 'nowrap'}}>下载新生手册</span>}/>
+                    </QueueAnim>
+                </div>
+                <Modal
+                    title="已复制本文链接到剪贴板"
+                    visible={this.state.shareLinkVisible}
+                    onCancel={() => {
+                        this.setState({shareLinkVisible: false});
+                    }}
+                    footer={
+                        <Button type="primary" onClick={() => {
+                            this.setState({shareLinkVisible: false});
+                        }}>
+                            OK
+                        </Button>
+                    }
+                >
+                    <Input value={window.location.href} onFocus={(event) => {
+                        event.target.select();
+                    }}/>
+                </Modal>
+            </div>
+        );
+    }
+}
