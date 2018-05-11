3913f667fe78582a805755b45d4f9d1dc6040dcc
Zian Ke <kezian@umich.edu>
2018-05-11 17:06:25 -0400

    npm run build

--- server/fh/api/index.py
+++ server/fh/api/index.py
@@ -8,3 +8,10 @@ from collections import OrderedDict
 def get_article_content():
     title = flask.request.args.get('title')
     return fh.model.get_article_content(title)
+
+@fh.app.route('/api/freshman-handbook/search/', methods=['GET'])
+def get_search_results():
+    # input: a string of user input keywords
+    # output: e.g. [['Title', '<p>content</p><p>content</p>'], ['Title', '<p>content</p><p>content</p>']]
+    keywords = flask.request.args.get('q')
+    return json.dumps([['Card Title', '<p>Card content</p><p>Card content</p><p>Card content</p>']])
