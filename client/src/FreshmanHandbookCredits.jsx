bc624908f65957413c78932dc003836108e226bf
kezian <kezian@umich.edu>
2018-05-16 13:43:54 -0400

    barcode vertical align center

--- client/src/FreshmanHandbookCredits.jsx
+++ client/src/FreshmanHandbookCredits.jsx
@@ -9,6 +9,16 @@ const $ = require('jquery');
 const apiRootPath = '/api/freshman-handbook/';
 // const apiRootPath = 'http://localhost:8002/api/freshman-handbook/';
 
+const centerParentStyle = {
+    position: 'relative'
+};
+
+const centerChildStyle = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+};
 
 const boxShadowStyle = {
     boxShadow: '0 0 6px rgba(0,0,0,0.16)'
@@ -16,6 +26,7 @@ const boxShadowStyle = {
 
 const containerStyle = Object.assign({
     display: 'inline-block',
+    verticalAlign: 'top',
     width: '50%',
     height: '100%',
     backgroundColor: '#ffffff',
@@ -24,6 +35,7 @@ const containerStyle = Object.assign({
 
 const containerStyleBarcode = Object.assign({
     display: 'inline-block',
+    verticalAlign: 'top',
     marginLeft: 50,
     width: 'calc(50% - 50px)',
     height: '100%',
@@ -71,15 +83,13 @@ class FreshmanHandbookCredits extends React.Component {
                     </div>
                 </div>
                 <div style={this.props.width > 992 ? containerStyleBarcode : {display: 'none'}}>
-                    <div style={{width: '100%', height: 80}}><h1>
-                        贡献人员
-                    </h1></div>
-                    <div style={{width: '100%', height: 'calc(100% - 80px)'}}>
-                        <Scrollbars style={{width: '100%', height: '100%'}}>
-                            <div style={{overflowWrap: 'break-word', wordWrap: 'break-word'}}>
-                                Content
-                            </div>
-                        </Scrollbars>
+                    <div style={Object.assign({height: '100%', width: '100%'}, centerParentStyle)}>
+                        <div style={Object.assign({width: '100%', textAlign: 'center'}, centerChildStyle)}>
+                            <h1>
+                                新生小助手
+                            </h1>
+
+                        </div>
                     </div>
                 </div>
             </div>
