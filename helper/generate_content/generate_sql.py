aa96c2b6f73568e885335f0f1c90c0c2793426b1
jasonzhao1998 <jasonyz@umich.edu>
2018-05-24 17:01:05 +0800

    update 学费

--- helper/generate_content/generate_sql.py
+++ helper/generate_content/generate_sql.py
@@ -22,7 +22,7 @@ hierarchy = [
             ['体检 & 疫苗 & 医疗保险', [
                 ['体检和疫苗', 0],
                 ['医疗保险', 0]]],
-            ['学费', 0]]],
+            ['学费+housing dll', 0]]],
         ['入学指南', [
             ['Mandatory Immigration Check-In', 0],
             ['International Orientation', 0],
